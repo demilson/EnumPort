13a75fd5c77d49f93e551c3bcf5d1dec813db2da
demilson <demilsonmontilva@gmail.com>
2021-06-14 20:53:41 -0400

    Update Discovery.sh

--- Discovery.sh
+++ Discovery.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+funtion ctrl_(){
+	echo -e "\n\n[*] Movistar VE RedTeam \n"
+	tput cnorm; exit 0
+}
+
 for i in $(seq 1 255); do
 	timeout 1 bash -c "ping -c 1 segmento.$i" > /dev/null && echo "segmento.$i - Activo" &
 done; wait
+
+tput cnorm
